35ec373ffd3d2aa43e22dae3155ba714fe3a8773
Adilson19 <adilsonsousaas82@gmail.com>
2025-05-16 17:09:20 +0100

    feat: implementando o dados do dashboard nos controles e na view

--- app/Http/Controllers/Auth/DashboardController.php
+++ app/Http/Controllers/Auth/DashboardController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\Category;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
     //
     public function dashboard()
     {
-        return view('auth.dashboard');
+        $postsCount = Post::count();
+        $categoriesCount = Category::count();
+        $userCount = User::count();
+        return view('auth.dashboard', compact('postsCount', 'categoriesCount', 'userCount'));
     }
 }
